38020843f41bc59495c99f5d443bf96bdc318ff7
Tamir Levi <tamir@apiiro.com>
2025-02-25 19:31:37 +0200

    .

---
+++ input-validation/src/test/java/com/example/inputvalidation/InputValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inputvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InputValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
