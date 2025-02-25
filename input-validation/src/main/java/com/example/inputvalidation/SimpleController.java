5a46ae4276c3030684110635e25170aec87030bf
Tamir Levi <tamir@apiiro.com>
2025-02-25 19:42:42 +0200

    unvalidated param

--- input-validation/src/main/java/com/example/inputvalidation/SimpleController.java
+++ input-validation/src/main/java/com/example/inputvalidation/SimpleController.java
@@ -2,13 +2,14 @@ package com.example.inputvalidation;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class SimpleController {
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, World!";
+    public String sayHello(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 }
