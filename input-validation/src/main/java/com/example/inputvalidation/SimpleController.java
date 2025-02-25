91a4385c15b3d38be89964be2448b4306cb455f1
Tamir Levi <tamir@apiiro.com>
2025-02-25 19:56:24 +0200

    .

--- input-validation/src/main/java/com/example/inputvalidation/SimpleController.java
+++ input-validation/src/main/java/com/example/inputvalidation/SimpleController.java
@@ -5,11 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Min;
+
 @RestController
 @RequestMapping("/api")
 public class SimpleController {
     @GetMapping("/hello")
     public String sayHello(@RequestParam String name) {
         return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/age")
+    public String validateAge(@RequestParam @Min(18) int age) {
+        return "Your age is valid: " + age;
     }
 }
